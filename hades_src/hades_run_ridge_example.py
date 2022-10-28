7f483b5a76fe0fb4a446d420a2759daa40a63fe3
wulwife <francesco.grigoli@unipi.it>
2022-10-28 12:08:33 +0900

    bug corrected

--- hades_src/hades_run_ridge_example.py
+++ hades_src/hades_run_ridge_example.py
@@ -9,16 +9,17 @@ stations=['WBS','WMF']
 Vp=6000
 Vs=Vp/1.73
 hobj=hades_input(data_path,input_file,sta_file)
-output_folder='./RIDGECREST'
+output_folder='./RIDGECREST/'
 mode='classic'
 
 if mode=='classic':
     hobj.distance_calculation(Vp,Vs,stations)
     hloc=hades_location(hobj,output_folder)
-    hloc.location(hobj,master=False,fixed=False)
+    hloc.location(out_file,master=False,fixed=False)
 elif mode=='master':
     hobj.relative_frame(Vp,Vs,stations,y_ref=-1,z_ref=-1,fixed_depth=1)
     hobj.distance_calculation(Vp,Vs,stations)
-    hloc=hades_location(hobj,master=True,fixed=False)
+    hloc=hades_location(hobj,output_folder)
+    hloc.location(out_file,master=False,fixed=False)
 else:
     print('mode must be "classic" or "master"!!!')
